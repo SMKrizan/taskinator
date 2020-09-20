1f740ae06e1b14df0be5ef22a12da97b90583ef6
smkrizan <smkrizan@gmail.com>
2020-09-20 17:55:30 -0500

    completed feature/add-task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
-
-var taskItemE1 = document.createElement("li");
-taskItemE1.textContent = "hello";
-taskItemE1.className = "task-item";
-
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
-tasksToDoE1.appendChild(taskItemE1);
+
+var createTaskHandler = function() {
+    var taskItemE1 = document.createElement("li");
+    taskItemE1.className = "task-item";
+    taskItemE1.textContent = "This is a new task.";
+    tasksToDoE1.appendChild(taskItemE1);
+}
+
+buttonE1.addEventListener("click", createTaskHandler);
+
+
