338041337f33cbdf2dc5658bf94f722e5dc174dd
smkrizan <smkrizan@gmail.com>
2020-09-21 14:30:32 -0500

    completed taskName and taskType entry to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var formE1 = document.querySelector("#task-form");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
 
@@ -9,25 +9,22 @@ var createTaskHandler = function(event) {
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
     // create list item
-    var taskItemE1 = document.createElement("li");
-    taskItemE1.className = "task-item";
+    var taskItemEl = document.createElement("li");
+    taskItemEl.className = "task-item";
 
     // create div to hold task info and add to list item
-    var taskInfoE1 = document.createElement("div");
+    var taskInfoEl = document.createElement("div");
     // give newly created div a class name
-    taskInfoE1.className = 'task-info';
-    // add HTML content to newly created div
-    // taskInfoE1.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.className = 'task-info';
+    // add HTML content to newly created div; will be read as an HTML tag and rendered as an HTML element in the DOM; the h3 and span tags could also be created as separate variables and added individually as elements in the DOM
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
     
-    taskItemE1.appendChild(taskInfoE1);
+    taskItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list
-    tasksToDoE1.appendChild(taskItemE1);
-
-    taskItemE1.textContent = taskNameInput;
-    tasksToDoE1.appendChild(taskItemE1);
+    tasksToDoEl.appendChild(taskItemEl);
 }
 
-formE1.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
