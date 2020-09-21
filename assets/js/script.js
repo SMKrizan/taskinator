7a102128be742d43fd94e4412680d004e6d68b24
smkrizan <smkrizan@gmail.com>
2020-09-20 20:06:23 -0500

    submission form updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var taskItemE1 = document.createElement("li");
     taskItemE1.className = "task-item";
     taskItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(taskItemE1);
 }
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
 
 
